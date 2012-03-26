abdf3b1d51fa1878f649f945cabead3577994fd4
Andre Mendonça <albarbosa@sp.r7.com>
2012-03-26 20:40:25 -0300

    Adding a simplest way to add async scripts

---
+++ simplest-lazy-load.js
@@ -0,0 +1,21 @@
+var load = function (id, src, callback) {
+  callback = callback || function () {};
+
+  var hasScript = !!(document.getElementById(id));
+  if (!hasScript) {
+    var node = document.createElement("script");
+    node.id = id;
+    node.src = src;
+    node.onload = callback;
+    //if IE
+    node.onreadystatechange = function () {
+      if (/loaded|complete/.test(node.readyState)) {
+        node.onreadystatechange = null;
+        callback();
+      }
+    };
+    document.getElementsByTagName('head')[0].appendChild(node);
+  } else {
+    callback();
+  }
+};
