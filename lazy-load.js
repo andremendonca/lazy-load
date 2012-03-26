be6bcd4a86cab02b4cbbb3b893539d4a5353b48a
Andre Mendonça <albarbosa@sp.r7.com>
2012-03-26 20:45:56 -0300

    Setting some attributes and methods as private

--- lazy-load.js
+++ lazy-load.js
@@ -1,37 +1,40 @@
-var Lazy = {
-  queue: [],
-  enqueue: function (id, src, callback) {
-    this.queue.push(Array.prototype.slice.call(arguments));
-    if (this.queue.length === 1) {
-      this.load();
-    }
-  },
-  load: function () {
-    var script = this.queue.shift(),
-        id = script[0],
-        src = script[1],
-        callback = script[2] || function () {};
+var Lazy = (function (doc) {
+  var queue = [],
+      load = function () {
+        var script = queue.shift(),
+            id = script[0],
+            src = script[1],
+            callback = script[2] || function () {};
 
-    var hasScript = !!(document.getElementById(id));
-    if (!hasScript) {
-      var node = document.createElement("script");
-      node.id = id;
-      node.src = src;
-      node.onload = callback;
-      //if IE
-      node.onreadystatechange = function () {
-        if (/loaded|complete/.test(node.readyState)) {
-          node.onreadystatechange = null;
+        var hasScript = !!(doc.getElementById(id));
+        if (!hasScript) {
+          var node = doc.createElement("script");
+          node.id = id;
+          node.src = src;
+          node.onload = callback;
+          //if IE
+          node.onreadystatechange = function () {
+            if (/loaded|complete/.test(node.readyState)) {
+              node.onreadystatechange = null;
+              callback();
+            }
+          };
+          doc.getElementsByTagName('head')[0].appendChild(node);
+        } else {
           callback();
         }
-      };
-      document.getElementsByTagName('head')[0].appendChild(node);
-    } else {
-      callback();
-    }
 
-    if (this.queue.length === 1) {
-      this.load();
+        if (queue.length === 1) {
+          load();
+        }
+      };
+
+  return {
+    enqueue: function (id, src, callback) {
+      queue.push(Array.prototype.slice.call(arguments));
+      if (queue.length === 1) {
+        load();
+      }
     }
-  }
-};
+  };
+})(document);
